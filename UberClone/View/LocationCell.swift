bfc45624348a57d9386baa8ad682002c52e05269
Saqib Bhatti <sqb_bhatti@yahoo.com>
2024-06-04 11:09:40 +0800

    Home screen with Location Input view and tableView design is done

---
+++ UberClone/View/LocationCell.swift
@@ -0,0 +1,23 @@
+//
+//  LocationCell.swift
+//  UberClone
+//
+//  Created by Saqib Bhatti on 3/6/24.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
