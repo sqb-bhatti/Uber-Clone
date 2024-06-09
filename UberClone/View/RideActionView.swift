d2366b746e246da123cdf72ee111d1da02a1a687
Saqib Bhatti <sqb_bhatti@yahoo.com>
2024-06-09 18:36:34 +0800

    MapView zoom fit Annotations

---
+++ UberClone/View/RideActionView.swift
@@ -0,0 +1,20 @@
+//
+//  RideActionView.swift
+//  UberClone
+//
+//  Created by Saqib Bhatti on 9/6/24.
+//
+
+import UIKit
+
+class RideActionView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
